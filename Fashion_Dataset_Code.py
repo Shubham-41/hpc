ba166a3abe4fb90bdf1922faf3c42cb8b0241ff0
LIPANE SHUBHAM SUNIL <90514327+Shubham-41@users.noreply.github.com>
2024-04-25 01:57:54 +0530

    Fashion_Dataset_Code.py

--- Fashion_Dataset_Code.py
+++ Fashion_Dataset_Code.py
@@ -1,30 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
 
 import tensorflow as tf
 
 image_data = tf.keras.datasets.fashion_mnist
 
 
-# In[5]:
-
-
 (train_images,train_labels),(test_images,test_labels) = image_data.load_data()
-
-
-# In[6]:
 
 
 train_images[0]
 
 # view of list of list 
-
-
-# In[7]:
-
 
 import matplotlib.pyplot as plt
 
@@ -35,28 +20,13 @@ train_labels[0]
 
 # # Preprocessing
 
-# In[8]:
-
-
 train_images = train_images / 255.0
 
 test_images = test_images / 255.0
 
-
-# In[9]:
-
-
 train_images[0]
 
-
-# In[10]:
-
-
 from tensorflow import keras
-
-
-# In[11]:
-
 
 model = tf.keras.Sequential()
 
@@ -70,50 +40,25 @@ model.add(keras.layers.Dense(128,activation="relu"))
 # output of the network
 model.add(keras.layers.Dense(10,activation='softmax'))
 
-
-# In[12]:
-
-
 # how it is affecting the network to make the better predictions to optimize it
 
 model.compile(optimizer="adam",loss='sparse_categorical_crossentropy',
              metrics=['accuracy'])
 
-
-
-# In[13]:
-
-
 model.fit(train_images,train_labels,epochs=10,verbose=1)
-
-
-# In[14]:
-
 
 model.predict(test_images)[0]
 
 # list of 10 numbers 
 # represent the probability of output
 
-
-# In[15]:
-
-
 test_labels[0]
-
-
-# In[16]:
-
 
 # Evaluate The model
 
 test_loss, test_acc = model.evaluate(test_images,test_labels)
 
 print('Test Accuracy:', test_acc)
-
-
-# In[17]:
-
 
 # Make Predictions
 
@@ -122,10 +67,6 @@ import numpy as np
 predictions = model.predict(test_images)
 
 predicted_labels = np.argmax(predictions,axis=1)
-
-
-# In[18]:
-
 
 # Show some example images and their predicted labels
 num_rows = 5
@@ -143,22 +84,4 @@ for i in range(num_images):
     plt.tight_layout()
     plt.title(f"Predicted label: {predicted_labels[i]}")
     plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
